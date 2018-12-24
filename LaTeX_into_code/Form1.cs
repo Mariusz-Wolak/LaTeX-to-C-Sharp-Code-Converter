4f0efd6b3c7f5cd22f8db369ed665f6664db123c
Mariusz-Wolak <Mariusz.Wolak@yahoo.com>
2018-12-24 17:34:57 +0100

    idk what is goin on here

--- LaTeX_into_code/Form1.cs
+++ LaTeX_into_code/Form1.cs
@@ -14,6 +14,10 @@ namespace LaTeX_into_code
     {
         string newClass;
         string paramsLine;
+        string[] operations = { "*", "+" };
+        int addOperatorsNum = 0;
+        string[] splitString;
+        string[] wholeAdditionArray;
 
         public Form1()
         {
@@ -34,28 +38,95 @@ namespace LaTeX_into_code
             myStreamWriter.Write(Convert.ToString(newClass));
             myStreamWriter.Close();
 
-            //zrobic funkcje Addition, Square, Sin itd. i wykrywac case'ami w zaleznosci od znaku np "+", "^"
+            insertBody(parse(RTB_txt));
 
             //cos w stylu equality(Ac,add(multiply(2,multiply(PI,multiply(r,h))),multiply(2,multiply(PI,multiply(r,r)))))
 
             //wykrywanie znakow i kolejnosci dzialan (* wazniejsze niz +, chyba ze nawias), np wykryje "+", to przesyla lewa i prawa czesc do dodawania, tam w dodawaniu tez musi wykrywac i jesli jest np. "^",
             // to znowu bierze lewa i prawa, jakas rekurencja do tego moze albo przenoszenie tych stringow i pamietanie dzialan
-            
-        
 
+            //robic znak po znaku, drzewo ze stringa, uzytkownik wybiera nazwe funkcji, zaznacza czy statyczna, apend jakby cos dopisac?
+
+            //zeby bylo latwiej znak po znaku, moge zrobic Replace z /sqrt na np. P, albo sprawdzac czy jesli kolejne znaki po "/" to "sqrt", to wtedy jest
+            //pierwiastek
 
             MessageBox.Show(fileName+" has been created.");
         }
 
-        void insertBody(string addedCode)
+        void insertHeader()
+        {
+
+        }
+
+        void insertBody(string codeToAdd)
         {
             int myIndex = newClass.IndexOf("//insertingBody");
-            newClass = newClass.Insert(myIndex, addedCode+"\n\t\t\t");
+            newClass = newClass.Insert(myIndex, codeToAdd+"\n\t\t\t");
         }
-
-        void insertParams()
+        
+        string parse(string latexToParse)
         {
+            string declarationString;
+            string returnString;
+            
+            
+            //addition (add to Addition in MathOperations?)
+            //if (latexToParse.Contains("+"))
+            //{
+            splitString = latexToParse.Split('+');
+            addOperatorsNum++;
 
+            //for(int i=0; i < splitString.Length; i++)
+            //{
+            //    wholeAdditionArray[i] = splitString[i];
+            //}
+
+            MessageBox.Show("splitString[0] = " + splitString[0]);
+
+            while (splitString[addOperatorsNum].Contains("+"))
+            {
+                MessageBox.Show("jest plus, prawa strona splitStringa: "+splitString[addOperatorsNum]);
+                
+                splitString = splitString[addOperatorsNum].Split('+');
+                
+                addOperatorsNum++;
+                MessageBox.Show("Po kolejnym splicie: " + splitString[addOperatorsNum]);
+            }
+            //}
+
+            
+            //jesli jest "=", to bierzemy tylko prawa czesc
+            //switch albo if albo bool do wykrycia z txtParse jakie sa znaki i na podstawie tego wykonac dzialanie (zadbac o kolejnosc matematyczna)
+            
+
+            
+
+            string doTestu="";
+
+            for(int i=0; i<addOperatorsNum; i++)
+            {
+                doTestu += "/n"+splitString[i];
+            }
+
+            
+
+            return doTestu;
         }
+
+        //void additionCounter(string toParse, char search)
+        //{
+        //    foreach (char c in toParse)
+        //    {
+        //        if (c == search)
+        //        {
+        //            addOperatorsNum++;
+        //        }
+        //    }
+
+        //    for (int i = 0; i < addOperatorsNum; i++)
+        //    {
+        //        splitString[addOperatorsNum] = 
+        //    }
+        //}
     }
 }
---
+++ LaTeX_into_code/MathOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaTeX_into_code
+{
+    class MathOperations
+    {
+
+
+        //string multiplication(string declarationString, string returnString, string arg1, string arg2)
+        //{
+        //    declarationString += "/n/tdouble " + arg1 + ";/n/tdouble " + arg2;
+        //    returnString += arg1 + "*" + arg2;
+
+        //    return wholeString;
+        //}
+
+        //string addition(string wholeString, string arg1, string arg2)
+        //{
+        //    wholeString += arg1 + "+" + arg2;
+        //    return wholeString;
+        //}
+    }
+}
