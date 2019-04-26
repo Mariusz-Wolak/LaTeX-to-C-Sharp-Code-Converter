2f079815a808e0798baf7bb9fa5f4fbcb741f138
Mariusz-Wolak <Mariusz.Wolak@yahoo.com>
2019-04-26 19:33:07 +0200

    update repository

---
+++ LaTeX_into_code/Equation.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace LaTeX_into_code
+{
+    class Equation
+    {
+        public string Latex_equation { get; set; }
+        public string Cs_equation { get; set; }
+
+        public string[] latex_operators = { "+", "-", "*", "/", @"\times", "^",  @"\frac", "{", "}", "(", ")", @"\pi", @"\ln()", @"\sin()", @"\cos()", @"\tan()", @"\cot()" };
+        
+        public List<string> Variables = new List<string>();
+        
+
+        public void convert_fraction(string equation)
+        {
+            string cs_fraction ="";
+            string newFrac ="";
+            string[] frac_operators = { @"\frac", "{", "}" };
+            
+            string pattern = @"\b\w+rac\b";
+            foreach (Match match in Regex.Matches(equation, pattern))
+            {
+                newFrac = "";
+                int open_brackets_counter = 0;
+                int closed_brackets_counter = 0;
+
+                foreach (char c in equation.Substring(match.Index - 1))
+                {
+                    newFrac += c;
+
+                    if (c == '{')
+                    {
+                        open_brackets_counter++;
+                    }
+                    if (c == '}')
+                    {
+                        closed_brackets_counter++;
+                    }
+                    if (open_brackets_counter == closed_brackets_counter && open_brackets_counter > 1)
+                    {
+                        break;
+                    }
+
+                }
+
+                cs_fraction = newFrac;
+                cs_fraction = cs_fraction.Replace("}{", "/");
+
+                for (int i = 0; i < frac_operators.Length; i++)
+                {
+                    cs_fraction = cs_fraction.Replace(frac_operators[i], " ");
+                }
+
+                cs_fraction = cs_fraction.Trim();
+                cs_fraction = "(" + cs_fraction + ")";
+                cs_fraction = cs_fraction.Insert(cs_fraction.IndexOf("/"), ")");
+                cs_fraction = cs_fraction.Insert(cs_fraction.IndexOf("/")+1, "(");
+                
+
+                this.Cs_equation = this.Cs_equation.Replace(newFrac, cs_fraction);
+            }
+        }
+
+        public void convert_exponentiation(string equation)
+        {
+            string cs_exp = "";
+            string new_exp = "";
+            string[] exp_operators = { "^", "{", "}" };
+            string my_base;
+            string my_exponent;
+
+            List<int> indexes = equation.AllIndexesOf("^");
+
+            for (int i=0; i<indexes.Count; i++)
+            {
+                int open_brackets_counter = 0;
+                int closed_brackets_counter = 0;
+                my_base = "";
+                my_exponent = "";
+
+                if(equation.Substring(indexes[i]-1,1) == ")")
+                {
+                    int counter = 1;
+                    char c;
+
+                    do
+                    {
+                        c = Convert.ToChar(equation.Substring(indexes[i] - counter, 1));
+                        my_base = c + my_base;
+
+                        if (c == ')')
+                        {
+                            closed_brackets_counter++;
+                        }
+                        if (c == '(')
+                        {
+                            open_brackets_counter++;
+                        }
+
+                        counter++;
+                    } while (open_brackets_counter != closed_brackets_counter);
+                }
+                else
+                {
+                    my_base = equation.Substring(indexes[i] - 1, 1);
+                }
+
+                open_brackets_counter = 0;
+                closed_brackets_counter = 0;
+
+                foreach (char c in equation.Substring(indexes[i] + 1))
+                {
+                    my_exponent += c;
+                    
+                    if (c == '{')
+                    {
+                        open_brackets_counter++;
+                    }
+                    if (c == '}')
+                    {
+                        closed_brackets_counter++;
+                    }
+                    if (open_brackets_counter == closed_brackets_counter && open_brackets_counter > 0)
+                    {
+                        break;
+                    }
+                }
+
+                new_exp = my_base + "^" + my_exponent;
+
+                my_base = my_base.Trim('(');
+                my_base = my_base.Trim(')');
+                my_exponent = my_exponent.Trim('{');
+                my_exponent = my_exponent.Trim('}');
+
+                cs_exp = "Pow(" + my_base + "," + my_exponent + ")";
+
+                this.Cs_equation = this.Cs_equation.Replace(new_exp, cs_exp);
+            }
+        }
+
+        public void convert_functions(string equation)
+        {
+            string cs_func = "";
+            string new_func = "";
+            string[] functions = { @"\sin", @"\cos", @"\tan", @"\cot", @"\ln" };
+
+            foreach(string func in functions)
+            {
+                List<int> indexes = equation.AllIndexesOf(func);
+
+                for (int i = 0; i < indexes.Count; i++)
+                {
+                    int open_brackets_counter = 0;
+                    int closed_brackets_counter = 0;
+                    new_func = "";
+                    foreach (char c in equation.Substring(indexes[i]))
+                    {
+                        new_func += c;
+
+                        if (c == '(')
+                        {
+                            open_brackets_counter++;
+                        }
+                        if (c == ')')
+                        {
+                            closed_brackets_counter++;
+                        }
+                        if (open_brackets_counter == closed_brackets_counter && open_brackets_counter > 0)
+                        {
+                            break;
+                        }
+                    }
+                    
+                    cs_func = "Math." + StringExtensions.FirstCharToUpper(func.Substring(1));
+                    cs_func = new_func.Replace(func, cs_func);
+                    if (func == @"\cot")
+                    {
+                        cs_func = "(1/" + cs_func.Replace("Cot", "Tan") + ")";
+                    }
+                    if(func == @"\ln")
+                    {
+                        cs_func = cs_func.Replace("Ln", "Log");
+                    }
+                    this.Cs_equation = this.Cs_equation.Replace(new_func, cs_func);
+                }
+            }
+        }
+        
+        public void Latex_to_CS()
+        {
+            this.convert_fraction(this.Latex_equation);
+            this.Cs_equation = this.Cs_equation.Replace(@"\times", "*");
+            this.Cs_equation = this.Cs_equation.Replace(@"\pi", "Math.PI");
+            this.convert_exponentiation(this.Cs_equation);
+            this.convert_functions(this.Cs_equation);
+
+            this.Cs_equation = Regex.Replace(this.Cs_equation, @"\s", "");
+        }
+
+        
+    }
+
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input)
+        {
+            switch (input)
+            {
+                case null: throw new ArgumentNullException(nameof(input));
+                case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
+                default: return input.First().ToString().ToUpper() + input.Substring(1);
+            }
+        }
+    }
+
+}
--- LaTeX_into_code/Form1.Designer.cs
+++ LaTeX_into_code/Form1.Designer.cs
@@ -44,18 +44,18 @@
             // richTextBox1
             // 
             this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.richTextBox1.Location = new System.Drawing.Point(105, 212);
+            this.richTextBox1.Location = new System.Drawing.Point(99, 216);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(518, 38);
+            this.richTextBox1.Size = new System.Drawing.Size(614, 38);
             this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "a+2-c++3.14-e";
+            this.richTextBox1.Text = "\\frac{1}{\\frac{x}{n}}+\\ln(x+1)-\\pi-(3*y)^{2/n}\\times\\cot(30)";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.button1.Location = new System.Drawing.Point(650, 196);
+            this.button1.Location = new System.Drawing.Point(719, 200);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 63);
+            this.button1.Size = new System.Drawing.Size(138, 63);
             this.button1.TabIndex = 1;
             this.button1.Text = "Convert to code!";
             this.button1.UseVisualStyleBackColor = true;
@@ -64,7 +64,7 @@
             // richTextBox2
             // 
             this.richTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.richTextBox2.Location = new System.Drawing.Point(347, 34);
+            this.richTextBox2.Location = new System.Drawing.Point(401, 31);
             this.richTextBox2.Name = "richTextBox2";
             this.richTextBox2.Size = new System.Drawing.Size(183, 33);
             this.richTextBox2.TabIndex = 2;
@@ -74,7 +74,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(527, 37);
+            this.label1.Location = new System.Drawing.Point(581, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 25);
             this.label1.TabIndex = 3;
@@ -84,7 +84,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(192, 83);
+            this.label2.Location = new System.Drawing.Point(246, 80);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(149, 25);
             this.label2.TabIndex = 5;
@@ -93,7 +93,7 @@
             // richTextBox3
             // 
             this.richTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.richTextBox3.Location = new System.Drawing.Point(347, 80);
+            this.richTextBox3.Location = new System.Drawing.Point(401, 77);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.Size = new System.Drawing.Size(183, 33);
             this.richTextBox3.TabIndex = 4;
@@ -103,7 +103,7 @@
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox1.Location = new System.Drawing.Point(266, 130);
+            this.checkBox1.Location = new System.Drawing.Point(320, 127);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(141, 29);
             this.checkBox1.TabIndex = 9;
@@ -115,7 +115,7 @@
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.checkBox2.Location = new System.Drawing.Point(266, 165);
+            this.checkBox2.Location = new System.Drawing.Point(320, 162);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(162, 29);
             this.checkBox2.TabIndex = 10;
@@ -126,7 +126,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(229, 37);
+            this.label3.Location = new System.Drawing.Point(283, 34);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(112, 25);
             this.label3.TabIndex = 11;
@@ -136,7 +136,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(12, 215);
+            this.label4.Location = new System.Drawing.Point(12, 219);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 25);
             this.label4.TabIndex = 12;
@@ -146,17 +146,17 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(21, 300);
+            this.label5.Location = new System.Drawing.Point(12, 305);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(213, 25);
             this.label5.TabIndex = 13;
-            this.label5.Text = "Possible operations?";
+            this.label5.Text = "Possible operations: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(794, 383);
+            this.ClientSize = new System.Drawing.Size(869, 364);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
